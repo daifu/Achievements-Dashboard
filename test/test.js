9bdd27dd2758b04629f80c066f0e89becda9d24a
daifu <daifu.ye@gmail.com>
2013-10-08 00:08:53 -0700

    rm test

--- test/test.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-
-describe('Achievements', function(){
-  describe('#init()', function(){
-    it('should have the at least 4 attributes', function(){
-      assert.equal(1,1);
-    })
-  })
-});
